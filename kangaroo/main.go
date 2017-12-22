da078b35c4ec4c944944ab20615fdb8d4d041900
Tatsuya Kitabatake <e2tod2@gmail.com>
2017-12-22 12:10:51 +0900

    kangaroo

---
+++ kangaroo/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+type kangaroo struct {
+	x int
+	v int
+}
+
+func main()  {
+	faster, slower := inputProcess()
+	ans := judge(faster, slower)
+
+	if ans {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+func judge(faster kangaroo, slower kangaroo) bool {
+	if faster.v == slower.v && faster.x != slower.x {
+		return false
+	}
+	if faster.x > slower.x {
+		return false
+	}
+
+	for faster.x <= slower.x {
+		if faster.x == slower.x {
+			return true
+		}
+		faster.x += faster.v
+		slower.x += slower.v
+	}
+
+	return false
+}
+
+func inputProcess() (kangaroo, kangaroo) {
+	var x1, x2, v1, v2 int
+	fmt.Scanf("%d", &x1)
+	fmt.Scanf("%d", &v1)
+	fmt.Scanf("%d", &x2)
+	fmt.Scanf("%d", &v2)
+
+	var faster, slower kangaroo
+	if v1 > v2 {
+		faster = kangaroo{x: x1, v: v1}
+		slower = kangaroo{x: x2, v: v2}
+	} else {
+		faster = kangaroo{x: x2, v: v2}
+		slower = kangaroo{x: x1, v: v1}
+	}
+	return faster, slower
+}
