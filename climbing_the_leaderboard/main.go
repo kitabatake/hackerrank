a7b264be746039031114bc92ddf822a2652d34fa
Tatsuya Kitabatake <e2tod2@gmail.com>
2017-12-23 12:51:28 +0900

    climbing the leaderboard

--- climbing_the_leaderboard/main.go
+++ climbing_the_leaderboard/main.go
@@ -1,4 +1,48 @@
+// https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 package main
 
+import "fmt"
+
 func main() {
+	playerScores , aliceScores := inputProcess()
+	currentRank := len(playerScores) + 1
+	currentIndex := len(playerScores) - 1
+
+	for _, score := range aliceScores {
+		// calculate current score's rank and next score's index
+		currentRank, currentIndex = calculateRankAndNextIndex(playerScores, score, currentIndex, currentRank)
+		fmt.Println(currentRank)
+	}
+}
+
+func calculateRankAndNextIndex(scores []int, score int, index int, rank int) (int, int) {
+	if index < 0 {
+		return 1, -1
+	} else if score < scores[index] {
+		return rank, index
+	} else {
+		return calculateRankAndNextIndex(scores, score, index - 1, rank - 1)
+	}
+}
+
+func inputProcess() (playerScores []int, aliceScores []int) {
+	var playersCount, levelsCount, score int
+	fmt.Scanf("%d", &playersCount)
+
+	prevScore := 1000000001
+	for i := 0; i < playersCount; i++ {
+		fmt.Scanf("%d", &score)
+		if score != prevScore {
+			playerScores = append(playerScores, score)
+			prevScore = score
+		}
+	}
+
+	fmt.Scanf("%d", &levelsCount)
+	aliceScores = make([]int, levelsCount)
+	for i := 0; i < levelsCount; i++ {
+		fmt.Scanf("%d", &aliceScores[i])
+	}
+
+	return
 }
